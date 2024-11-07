26acacd7c1d12a50d310682d33550c8889845efc
suhailasrulsani-git <124322527+suhailasrulsani-git@users.noreply.github.com>
2024-11-08 07:17:44 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,115 +1,11 @@
 let InputAddInputEL = document.getElementById("InputAddInputEL")
-let OptionCategoryEL = document.getElementById("OptionCategoryEL")
-let RoutineAccordionListEL = document.getElementById("Routine-AccordionListEL")
-let RoutineHeaderEL = document.getElementById("Routineheader")
 let Alert = document.getElementById("Alert")
+let OptionCategoryEL = document.getElementById("OptionCategoryEL")
+
 let Routine = []
 let Ibadah = []
 let NonRoutine = []
 let ToBuy = []
-
-function RenderBadge(task, taskname) {
-    const IncompleteCount = task.filter(item => item.completed === false).length;
-    console.log(`Total Items in ${taskname} is ${IncompleteCount}`)
-    console.log(RoutineHeaderEL)
-
-    let x = ""
-    x = `<button
-        class="accordion-button collapsed"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#flush-collapseOne"
-        aria-expanded="false"
-        aria-controls="flush-collapseOne"
-      >
-        ${taskname}<span class="badge rounded-pill text-bg-primary ms-2">${IncompleteCount}</span>
-      </button>`
-
-    RoutineHeaderEL.innerHTML = x
-}
-
-function RenderList(InputAddInputEL, OptionCategoryEL, categoryArray, categoryList) {
-    const randomId = Math.floor(1000000000 + Math.random() * 9000000000);
-    const taskObject = { taskname: OptionCategoryEL.value, id: randomId, text: InputAddInputEL.value, completed: false };
-
-    if (OptionCategoryEL.value === "Routine") {
-
-        //push into array
-        categoryArray.push(taskObject)
-        console.log(categoryArray)
-
-       RenderBadge(Routine, 'Routine')
-
-        //save to local storage
-        // localStorage.setItem("routine", JSON.stringify(Routine));
-
-        let ListItems = "";
-        categoryArray.forEach((Item) => {
-            ListItems += `<li id="Routine-${Item.id}" class="list-group-item ms-0 p-0 d-flex justify-content-between">
-              ${Item.text}
-              <div>
-                <input
-                  id="${Item.id}"
-                  onchange="CheckCheckbox(${Item.id})"
-                  class="form-check-input me-3"
-                  type="checkbox"
-                  value=""
-                /><button
-                  id="${Item.id}"
-                  onclick="Remove(${Item.id})"
-                  type="button"
-                  class="btn-close"
-                  aria-label="Close"
-                ></button>
-              </div>
-            </li>`
-        });
-        categoryList.innerHTML = ListItems
-    }
-}
-
-function CheckCheckbox(id) {
-    CheckBoxEL = document.getElementById(id)
-    IsChecked = CheckBoxEL.checked
-
-    console.log(id)
-
-    if (IsChecked) {
-        console.log(CheckBoxEL.checked)
-        const foundItem = [...Routine, ...NonRoutine, ...Ibadah]
-            .find(item => item.id === id);
-
-        if (foundItem) {
-            foundItem.completed = true
-            console.log(foundItem)
-        }
-
-        let element = document.getElementById(`Routine-${id}`);
-        if (element) {
-            element.classList.add("text-decoration-line-through")
-            RenderBadge(Routine, 'Routine')
-        }
-    }
-
-    else {
-        console.log(CheckBoxEL.checked)
-        const foundItem = [...Routine, ...NonRoutine, ...Ibadah]
-            .find(item => item.id === id);
-
-        if (foundItem) {
-            foundItem.completed = false
-            console.log(foundItem)
-            
-        }
-
-        let element = document.getElementById(`Routine-${id}`);
-        if (element) {
-            element.classList.remove("text-decoration-line-through")
-            RenderBadge(Routine, 'Routine')
-        }
-    }
-}
-
 
 function Add() {
     if (InputAddInputEL.value === "") {
@@ -117,7 +13,6 @@ function Add() {
             Please insert input!
         </div>`;
 
-        // Set a timeout to clear the alert after 3 seconds (3000 milliseconds)
         setTimeout(() => {
             Alert.innerHTML = "";
         }, 1000);
@@ -137,50 +32,149 @@ function Add() {
     }
 
     if (OptionCategoryEL.value === "Routine") {
-        RenderList(InputAddInputEL, OptionCategoryEL, Routine, RoutineAccordionListEL)
-        RenderBadge(Routine, 'Routine')
+        RenderList('Routine', Routine)
+        RenderBadge('Routine', Routine)
+    }
+
+    if (OptionCategoryEL.value === "Ibadah") {
+        RenderList('Ibadah', Ibadah)
+        RenderBadge('Ibadah', Ibadah)
+    }
+
+    if (OptionCategoryEL.value === "NonRoutine") {
+        RenderList('NonRoutine', NonRoutine)
+        RenderBadge('NonRoutine', NonRoutine)
+    }
+
+    if (OptionCategoryEL.value === "ToBuy") {
+        RenderList('ToBuy', ToBuy)
+        RenderBadge('ToBuy', ToBuy)
     }
 }
 
+function RenderList(Category, CategoryObject) {
+    const randomId = Math.floor(1000000000 + Math.random() * 9000000000);
+    const TaskObject = { id: randomId, text: InputAddInputEL.value, completed: false };
 
-function Remove(id) {
-    const foundItem = [...Routine, ...NonRoutine, ...Ibadah]
+    console.log(`You have selected category: ${Category}`)
+    console.log(`The input you want to add is: ${InputAddInputEL.value}`)
+
+    CategoryObject.push(TaskObject)
+
+    console.log(CategoryObject)
+
+    let AccordionListEL = `${Category}AccordionListEL`
+    // console.log(AccordionListEL)
+    let AccordionBody = document.getElementById(AccordionListEL)
+    // console.log(AccordionBody)
+
+    let ListItems = "";
+    CategoryObject.forEach((Item) => {
+        ListItems += `<li id=${Item.id} class="list-group-item ms-0 p-0 d-flex justify-content-between">
+              ${Item.text}
+
+              <div>
+                <input
+                  id=CheckBox-${Item.id}
+                  onchange="CheckCheckbox(${Item.id}, ${Category}, '${Category}')"
+                  class="form-check-input me-3"
+                  type="checkbox"
+                  value=""
+                /><button
+                  id=${Item.id}
+                  onclick="Remove(${Item.id}, ${Category}, '${Category}')"
+                  type="button"
+                  class="btn-close"
+                  aria-label="Close"
+                ></button>
+              </div>
+            </li>`
+
+        AccordionBody.innerHTML = ListItems
+    });
+
+    console.log(ListItems)
+
+}
+
+function RenderBadge(Category, CategoryObject) {
+    let x = `${Category}BadgeEL`
+    console.log(x)
+
+    let BadgeEL = document.getElementById(x)
+    console.log(BadgeEL)
+
+    console.log(CategoryObject)
+    const IncompleteCount = CategoryObject.filter(item => item.completed === false).length;
+
+    console.log(IncompleteCount)
+
+    BadgeEL.innerHTML = `<span class="badge rounded-pill text-bg-primary ms-2"> ${IncompleteCount} </span>`
+
+}
+
+function CheckCheckbox(id, CategoryObject, categoryName) {
+
+    let CheckBoxEL = document.getElementById(`CheckBox-${id}`)
+    console.log(CheckBoxEL)
+    console.log(CheckBoxEL.checked)
+
+    if (CheckBoxEL.checked === true) {
+        console.log(CategoryObject)
+        CategoryObject.forEach((Item) => {
+            if (Item.id === id) {
+                console.log(`Item.id is equal to ${id}`)
+                Item.completed = true
+                let StrikeThroughElement = document.getElementById(id)
+                console.log(StrikeThroughElement)
+                StrikeThroughElement.classList.add("text-decoration-line-through")
+            }
+        })
+        console.log(CategoryObject)
+    }
+
+    else {
+        console.log(CategoryObject)
+        CategoryObject.forEach((Item) => {
+            if (Item.id === id) {
+                Item.completed = false
+                let StrikeThroughElement = document.getElementById(id)
+                console.log(StrikeThroughElement)
+                StrikeThroughElement.classList.remove("text-decoration-line-through")
+            }
+        })
+        console.log(CategoryObject)
+    }
+    RenderBadge(categoryName, CategoryObject)
+}
+
+function Remove(id, CategoryObject, categoryName) {
+    const foundItem = [...Routine, ...NonRoutine, ...Ibadah, ...ToBuy]
         .find(item => item.id === id);
 
 
-    const sourceArray = [Routine, NonRoutine, Ibadah].find(array =>
+    const sourceArray = [Routine, NonRoutine, Ibadah, ToBuy].find(array =>
         array.some(item => item.id === id)
     );
-
+    console.log(foundItem)
     console.log(sourceArray)
 
     if (sourceArray) {
-        // Find the index in the correct array and remove the item
         const index = sourceArray.findIndex(item => item.id === id);
         console.log(index)
         if (index !== -1) {
             sourceArray.splice(index, 1);
-            console.log(`Item removed from the array`);
-        }
+            console.log(`Item removed from the ${CategoryObject}`);
 
-        console.log(Routine)
-
-        let RemoveElement = document.getElementById(`Routine-${id}`)
-
-        if (RemoveElement) {
+            let RemoveElement = document.getElementById(id)
+            console.log(RemoveElement)
             RemoveElement.remove()
-            RenderBadge(Routine, 'Routine')
+            RenderBadge(categoryName, CategoryObject)
         }
     }
 }
 
 
-// function Reset() {
-//     Routine = []
-//     Ibadah = []
-//     NonRoutine = []
-//     ToBuy = []
-//     localStorage.setItem("routine", JSON.stringify(Routine));
-//     localStorage.clear()
-// }
-// RenderBadge(Routine, 'Routine')
+function Reset() {
+
+}
